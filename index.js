42ebeffdac94566eaef211a538c86923ba288895
Andres Espinace <atarix.010101@gmail.com>
2017-03-10 18:42:41 -0300

    WIP

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 'use strict';
 
 const config = require('./config.json');
+const Git = require('./lib/git');
 const Gitlab = require('./lib/gitlab');
 
 console.log(config)
 
+const git = new Git(config);
 const gitlab = new Gitlab(config.gitlab);
 
-gitlab.issues(1775437)
-	.then(issues => issues.forEach(issue => console.log(issue)))
-	.catch(err => console.error(err));
+let p = Promise.resolve();
+
+const projectId = 1775437;
+const gitlabName = 'nedmedia/wp-radios';
+const githubName = 'wp-radios';
+
+const db = {};
+
+Promise.all([
+	gitlab.milestones(projectId),
+	gitlab.issues(projectId)
+])
+.then(results => {
+	db.milestones = results[0].length && results[0] || null;
+	db.issues = results[1].length && results[1] || null;
+	// NOTE: agregar db.wiki
+
+	return git.migrateRepo(gitlabName, githubName)
+})
+.then(result => {
+	debugger;
+})
+.catch(err => console.error(err));
