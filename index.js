f18b84324220fdeed7f832b9e1bce7e8cc64d992
saidgeek <saidgeek@Device-de-saidgeek.local>
2017-03-10 23:52:39 -0300

    Implement clone --mirror and wip refactoring gitlab lib

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
 'use strict';
 
+const fse = require('fs-extra-promise');
+const path = require('path');
 const config = require('./config.json');
 const Git = require('./lib/git');
 const Gitlab = require('./lib/gitlab');
 
-console.log(config)
-
-const git = new Git(config);
-const gitlab = new Gitlab(config.gitlab);
-
-let p = Promise.resolve();
 
 const projectId = 1775437;
 const gitlabName = 'nedmedia/wp-radios';
 const githubName = 'wp-radios';
-
 const db = {};
 
-Promise.all([
-	gitlab.milestones(projectId),
-	gitlab.issues(projectId)
-])
-.then(results => {
-	db.milestones = results[0].length && results[0] || null;
-	db.issues = results[1].length && results[1] || null;
-	// NOTE: agregar db.wiki
+const basePath = path.join(__dirname, 'repos', gitlabName);
+const git = new Git(basePath, config);
+const gitlab = new Gitlab(basePath, config.gitlab);
 
-	return git.migrateRepo(gitlabName, githubName)
-})
-.then(result => {
-	debugger;
-})
-.catch(err => console.error(err));
+fse.removeSync(path.join(basePath, 'git'));
+
+fse.ensureDirAsync(basePath)
+  .then(reposDir => Promise.all([
+    git.migrateRepo(gitlabName, githubName),
+    gitlab.milestones(projectId),
+    gitlab.issues(projectId)
+  ]))
+  .then(results => {
+    db.milestones = results[0].length && results[0] || null;
+    db.issues = results[1].length && results[1] || null;
+    // NOTE: agregar db.wiki
+    debugger;
+  })
+  .then(result => {
+    debugger;
+  })
+  .catch(err => console.error(err));
+
+
